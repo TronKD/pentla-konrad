56c3587b66d49a27aad68eaf58ec37586c3aa7aa
TronKD <62107446+TronKD@users.noreply.github.com>
2020-03-13 10:04:11 +0100

    Add files via upload

--- pentla konrad.cpp
+++ pentla konrad.cpp
@@ -1,62 +1,18 @@
-[Project]
-FileName=pentla konrad.cpp
-Name=konrad
-Type=1
-Ver=2
-ObjFiles=
-Includes=
-Libs=
-PrivateResource=
-ResourceIncludes=
-MakeIncludes=
-Compiler=
-CppCompiler=
-Linker=
-IsCpp=1
-Icon=
-ExeOutput=
-ObjectOutput=
-LogOutput=
-LogOutputEnabled=0
-OverrideOutput=0
-OverrideOutputName=
-HostApplication=
-UseCustomMakefile=0
-CustomMakefile=
-CommandLine=
-Folders=
-IncludeVersionInfo=0
-SupportXPThemes=0
-CompilerSet=0
-CompilerSettings=0000000000000000000000000
-UnitCount=1
+#include <iostream>
 
-[VersionInfo]
-Major=1
-Minor=0
-Release=0
-Build=0
-LanguageID=1033
-CharsetID=1252
-CompanyName=
-FileVersion=
-FileDescription=Developed using the Dev-C++ IDE
-InternalName=
-LegalCopyright=
-LegalTrademarks=
-OriginalFilename=
-ProductName=
-ProductVersion=
-AutoIncBuildNr=0
-SyncProduct=1
+using namespace std;
 
-[Unit1]
-FileName=..\Documents\main.cpp
-CompileCpp=1
-Folder=
-Compile=1
-Link=1
-Priority=1000
-OverrideBuildCmd=0
-BuildCmd=
+int t[521];
 
+int main() {
+	for (int i=1040; i>-1; i-=2){
+		t[i]=i;
+		t[0]=0;
+	
+	}
+	for (int i=1040;i>-1;i-=2){
+	cout<<i;
+		if(i!=0)cout<<",";
+	}
+		return 0;
+	}
